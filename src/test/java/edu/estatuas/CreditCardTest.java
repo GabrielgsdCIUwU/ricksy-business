4f280de7c95400ebdf5c7280f4163e7902df7992
GabrielgsdCIUwU <holagabriel321@gmail.com>
2025-04-24 14:08:38 +0200

    test(credit-card): Añadir casos test de pagar

--- src/test/java/edu/estatuas/CreditCardTest.java
+++ src/test/java/edu/estatuas/CreditCardTest.java
@@ -22,4 +22,18 @@ public class CreditCardTest {
         CreditCard card = new CreditCard("Abradolph Lincler", "4916119711304546");
         assertEquals("4916119711304546", card.number());
     }
+
+    @Test
+    public void testSuccesfulPay() {
+        CreditCard card = new CreditCard("Abradolph Lincler", "4916119711304546");
+        assertEquals(true, card.pay(1000.0));
+        assertEquals(2000.0, card.credit(), 0.01);
+    }
+
+    @Test
+    public void testUnsuccesfulPay() {
+        CreditCard card = new CreditCard("Abradolph Lincler", "4916119711304546");
+        assertEquals(false, card.pay(4000.0));
+        assertEquals(3000.0, card.credit(), 0.01);
+    }
 }
