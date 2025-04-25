25cc694ba0c86055009eb0302ea227686d3077c6
Gabrielgsd <104272301+GabrielgsdCIUwU@users.noreply.github.com>
2025-04-25 17:48:09 +0200

    feat(ufos-park): Añadir clase UfosPark

---
+++ src/main/java/edu/estatuas/guess_dispatchers/UfosPark.java
@@ -0,0 +1,63 @@
+package edu.estatuas.guess_dispatchers;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import edu.estatuas.CreditCard;
+import edu.estatuas.interfaces.GuessDispatcher;
+
+public class UfosPark implements GuessDispatcher {
+    private double fee = 500.0;
+    private final Map<String, String> flota = new HashMap<>();
+    private final List<String> creditCards = new ArrayList<>();
+
+    public void add(String vehicle) {
+        getFlota().put(vehicle, null);
+    }
+
+    Map<String, String> getFlota() {
+        return flota;
+    }
+
+    private List<String> getCreditCards() {
+        return creditCards;
+    }
+
+    private void addCreditCard(String creditCardNumber) {
+        creditCards.add(creditCardNumber);
+    }
+
+    public String getUfoOf(String creditNumber) {
+        for (Map.Entry<String, String> entry : getFlota().entrySet()) {
+            String vehicle = entry.getKey();
+            String entryCreditNumber = entry.getValue();
+            if (entryCreditNumber != null && entryCreditNumber.equals(creditNumber)) {
+                return vehicle;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void dispatch(CreditCard creditCard) {
+        String creditCardNumber = creditCard.number();
+        if (!getCreditCards().contains(creditCardNumber)) {
+            for (String ovni : getFlota().keySet()) {
+                if (getFlota().get(ovni) == null && creditCard.pay(fee)) {
+                    getFlota().put(ovni, creditCardNumber);
+                    addCreditCard(creditCardNumber);
+                    break;
+                }
+            }
+
+        }
+    }
+
+    @Override
+    public String toString() {
+        return getFlota().keySet().toString();
+    }
+
+}
