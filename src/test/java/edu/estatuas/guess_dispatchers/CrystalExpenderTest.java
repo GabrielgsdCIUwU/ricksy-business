a6be0f3809a820529c7afa26bf0297e1d35d44e4
Gabrielgsd <104272301+GabrielgsdCIUwU@users.noreply.github.com>
2025-05-02 12:15:51 +0200

    test(crystal-expender): Añadir NoStock y Paying tests

--- src/test/java/edu/estatuas/guess_dispatchers/CrystalExpenderTest.java
+++ src/test/java/edu/estatuas/guess_dispatchers/CrystalExpenderTest.java
@@ -38,4 +38,32 @@ public class CrystalExpenderTest {
         assertEquals(stock, expender.getStock());
         assertEquals(0.0, creditCard.credit(), 0.01);
     }
+    @Test
+    public void testNoStock() {
+        int stock = 0;
+
+        CrystalExpender expender = new CrystalExpender(stock, 50.0);
+
+        CreditCard creditCard = new CreditCard("Rick", "123456789");
+        expender.dispatch(creditCard);
+
+        assertEquals(stock, expender.getStock());
+    }
+
+    @Test
+    public void testPaying() {
+        CrystalExpender expender = new CrystalExpender(2, 50.0);
+
+        CreditCard rickCreditCard = new CreditCard("Rick", "123456789");
+        CreditCard mortyCreditCard = new CreditCard("Morty", "1234567890123456");
+        
+        expender.dispatch(rickCreditCard);
+
+        assertEquals(1, expender.getStock());
+
+        mortyCreditCard.pay(3000);
+
+        expender.dispatch(mortyCreditCard);
+        assertEquals(1, expender.getStock());
+    }
 }
